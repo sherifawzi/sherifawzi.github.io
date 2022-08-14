bf476e658e9f0a840ed34c23bab190f44df25e88
Sherifawzi <safatwork@hotmail.com>
2022-08-14 18:35:13 +0200

    Add files via upload
    Start common header and header with call to action

---
+++ SNRHeader.js
@@ -0,0 +1,30 @@
+const SNRHeaderTemplate = document.createElement('Template');
+
+SNRHeaderTemplate.innerHTML = `
+
+<div id="NavbarDIV" class="sBgClr06 sTxtClr03 sFontB">
+<div class="sContentW" >
+	<div class="w7 FL" >
+		<a href = "index.html" >
+			<img style="max-width:90% ; padding-top:5px ; " src="Pics/MainLogoPNG.png" >
+		</a>
+	</div>
+	<div class="w12 FR JM sNavItem">
+		<a href = "index.html#FAQ" class="sLink" >
+			FAQs
+		</a>
+	</div>
+	<div class="w12 FR JM sNavItem" >
+		<a href = "index.html#WHYSNR" class="sLink" >
+			Why SNR_ ?
+		</a>
+	</div>
+	<div class="w12 FR JM sNavItem" >
+		<a href = "Bots.html" class="sLink" >
+			Our bots
+		</a>
+	</div>
+</div>
+</div>
+
+` ; document.body.appendChild(SNRHeaderTemplate.content) ;
