cf0fcbe95056bf5f5e57b2934a1831c347f06faa
Sherifawzi <safatwork@hotmail.com>
2022-08-14 23:16:36 +0200

    Add files via upload
    add page title function

---
+++ SNRPageTitle.js
@@ -0,0 +1,19 @@
+const SNRPageTitle = document.createElement('Template');
+
+SNRPageTitle.innerHTML = `
+
+<div id="SectionDIV" class ="sBgClr05 sTxtClr03">
+<div class="sContentW" >
+	<div class="h100"></div>
+	<div class="JM sLandingTitle" >
+		<span id="title01" ></span>
+		<span id="title02" class="sShadow sTxtClr02" ></span>
+	</div>
+	<div class="sLine w40 FM h10" ></div>
+	<div class="Divider"></div>
+</div>
+<div class="Divider"></div>
+<div class="h100"></div>
+</div>
+
+` ; document.body.appendChild(SNRPageTitle.content) ;
