c1b9592ef0effe22899515cfc226538fefcb413d
Sherifawzi <safatwork@hotmail.com>
2022-08-15 08:37:59 +0200

    Add files via upload
    Add functions to pagetitle file

--- SNRPageTitle.js
+++ SNRPageTitle.js
@@ -1,19 +1,22 @@
 const SNRPageTitle = document.createElement('Template');
 
-SNRPageTitle.innerHTML = `
+	SNRPageTitle.innerHTML = `
 
-<div id="SectionDIV" class ="sBgClr05 sTxtClr03">
-<div class="sContentW" >
-	<div class="h100"></div>
-	<div class="JM sLandingTitle" >
-		<span id="title01" ></span>
-		<span id="title02" class="sShadow sTxtClr02" ></span>
+	<div id="SectionDIV" class ="sBgClr05 sTxtClr03">
+		<div class="sContentW" >
+			<div class="h100"></div>
+			<div class="JM sLandingTitle" >
+				<span id="tit01" ></span>
+				<span id="tit02" class="sShadow sTxtClr02" ></span>
+			</div>
+			<div class="sLine w40 FM h10" ></div>
+			<div class="Divider"></div>
+		</div>
+		<div class="Divider"></div>
+		<div class="h100"></div>
 	</div>
-	<div class="sLine w40 FM h10" ></div>
-	<div class="Divider"></div>
-</div>
-<div class="Divider"></div>
-<div class="h100"></div>
-</div>
 
-` ; document.body.appendChild(SNRPageTitle.content) ;
+	` ; 
+	document.body.appendChild(SNRPageTitle.content) ;
+	document.getElementById("tit01").innerHTML = tit01 ;
+	document.getElementById("tit02").innerHTML = tit02 ;
