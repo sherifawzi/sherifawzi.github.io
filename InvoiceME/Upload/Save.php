e25e047e2aaa21131a186735c8dda7e615f5bff3
Sherifawzi <safatwork@hotmail.com>
2023-03-20 11:03:24 +0100

    Add save script to test uploading to server

---
+++ InvoiceME/Upload/Save.php
@@ -0,0 +1,20 @@
+<?php
+// Get the data sent from VB6 application
+$name = $_POST['name'];
+$age = $_POST['age'];
+
+// Set the path to the text file
+$file_path = '/path/to/myfile.txt';
+
+// Open the file in append mode
+$file = fopen($file_path, 'a');
+
+// Write the data to the file
+fwrite($file, "$name,$age\n");
+
+// Close the file
+fclose($file);
+
+// Send a response back to the VB6 application
+echo "Data saved successfully";
+?>
