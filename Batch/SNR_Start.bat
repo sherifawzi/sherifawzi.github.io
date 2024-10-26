08287b032a770b42e98deff14aafc59922623a54
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2024-10-26 18:27:22 +0200

    Update SNR_Start.bat

--- Batch/SNR_Start.bat
+++ Batch/SNR_Start.bat
@@ -1,33 +1,33 @@
-   @echo off
-   setlocal
+@echo off
+setlocal
 
-   :: ------------------------------ File to download
-   ::set "snrbatch=Analyzer.bat"
-   ::set "snrbatch=Beacon.bat"
-   ::set "snrbatch=History.bat"
-   ::set "snrbatch=Receiver.bat"
-   ::set "snrbatch=restart.bat"
-   ::set "snrbatch=RevMag.bat"
-   ::set "snrbatch=RevMagLite.bat"
-   ::set "snrbatch=RevMagLive.bat"
-   ::set "snrbatch=Swapie.bat"
-   ::set "snrbatch=Tester.bat"
+:: ------------------------------ File to download
+::set "snrbatch=Analyzer.bat
+::set "snrbatch=Beacon.bat
+::set "snrbatch=History.bat
+:: set "snrbatch=Receiver.bat
+::set "snrbatch=restart.bat
+::set "snrbatch=RevMagLive.bat
+::set "snrbatch=Swapie.bat
+::set "snrbatch=Tester.bat
+set "snrbatch=RevHist.bat"
 
-   :: ------------------------------ Download URL
-   set "snrurl=https://sherifawzi.github.io/Batch/"
+:: ------------------------------ Download URL
+set "snrurl=https://sherifawzi.github.io/Batch/"
 
-   :: ------------------------------ Create Folders
-   ping -n 3 127.0.0.1 >nul
-   set "batchpath=%USERPROFILE%\Desktop\BAT"
-   md "%batchpath%" 2>nul
+:: ------------------------------ Create Folders
+ping -n 3 127.0.0.1 >nul
+set "batchpath=%USERPROFILE%\Desktop\BAT"
+md "%batchpath%" 2>nul
 
-   :: ------------------------------ Download from GitHub
-   ping -n 3 127.0.0.1 >nul
-   powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrbatch%' -OutFile '%batchpath%\%snrbatch%'"
+:: ------------------------------ Download from GitHub
+ping -n 3 127.0.0.1 >nul
+powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrbatch%' -OutFile '%batchpath%\%snrbatch%'"
 
-   :: ------------------------------ Start MT5s
-   ping -n 10 127.0.0.1 >nul
-   cd "%batchpath%" 2>nul
-   start %batchpath%\%snrbatch%
+:: ------------------------------ Start MT5s
+ping -n 10 127.0.0.1 >nul
+cd "%batchpath%" 2>nul
+start %batchpath%\%snrbatch%
 
-   endlocal
+endlocal
+exit
