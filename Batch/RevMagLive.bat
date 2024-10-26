0af596b0f82b03199ae64450e65b25ed3bceed0b
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2024-10-26 18:54:11 +0200

    Update RevMagLive.bat

--- Batch/RevMagLive.bat
+++ Batch/RevMagLive.bat
@@ -1,9 +1,6 @@
-   @echo off
-   setlocal
 
-   :: ------------------------------ Download Variables
-   set "snrbot=RevMagLive.ex5"
-   set "snrurl=https://sherifawzi.github.io/Tools/"
+@echo off
+setlocal
 
    :: ------------------------------ Create MT5 Folders
    ping -n 3 127.0.0.1 >nul
@@ -26,6 +23,14 @@
    set "mtbotpath4=%mtmainpath4%\MQL5\Experts"
    md "%mtbotpath4%" 2>nul
 
+   :: ------------------------------ Download URL
+   set "snrurl=https://sherifawzi.github.io/Tools/"
+
+
+
+   :: ------------------------------ Bot File Name
+   set "snrbot=RevMagLive.ex5"
+
    :: ------------------------------ Download from GitHub
    ping -n 3 127.0.0.1 >nul
    powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrbot%' -OutFile '%mtbotpath%\%snrbot%'"
@@ -40,21 +45,42 @@
    ping -n 5 127.0.0.1 >nul
    copy "%mtbotpath%\%snrbot%" "%mtbotpath4%"
 
+
+
+   :: ------------------------------ Bot File Name
+   set "snrbot=HistoryWriter.ex5"
+
+   :: ------------------------------ Download from GitHub
+   ping -n 3 127.0.0.1 >nul
+   powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrbot%' -OutFile '%mtbotpath%\%snrbot%'"
+
+   :: ------------------------------ Copy to other MT5s
+   ping -n 5 127.0.0.1 >nul
+   copy "%mtbotpath%\%snrbot%" "%mtbotpath2%"
+
+   ping -n 5 127.0.0.1 >nul
+   copy "%mtbotpath%\%snrbot%" "%mtbotpath3%"
+
+   ping -n 5 127.0.0.1 >nul
+   copy "%mtbotpath%\%snrbot%" "%mtbotpath4%"
+
+
+
    :: ------------------------------ Start MT5s
    ping -n 90 127.0.0.1 >nul
    cd "%mtmainpath%" 2>nul
-   %COMSPEC% /C start %mtmainpath%\terminal64.exe /portable
+   ::%COMSPEC% /C start %mtmainpath%\terminal64.exe /portable
 
    ping -n 90 127.0.0.1 >nul
    cd "%mtmainpath2%" 2>nul
-   %COMSPEC% /C start %mtmainpath2%\terminal64.exe /portable
+   ::%COMSPEC% /C start %mtmainpath2%\terminal64.exe /portable
 
    ping -n 90 127.0.0.1 >nul
    cd "%mtmainpath3%" 2>nul
-   %COMSPEC% /C start %mtmainpath3%\terminal64.exe /portable
+   ::%COMSPEC% /C start %mtmainpath3%\terminal64.exe /portable
 
    ping -n 90 127.0.0.1 >nul
    cd "%mtmainpath4%" 2>nul
-   %COMSPEC% /C start %mtmainpath4%\terminal64.exe /portable
+   ::%COMSPEC% /C start %mtmainpath4%\terminal64.exe /portable
 
-   endlocal
+endlocal
