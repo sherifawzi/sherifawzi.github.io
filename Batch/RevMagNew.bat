dbbf2487e88986d612a0fcf7cc4db084dc5b5d01
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2025-05-06 16:52:07 +0200

    Create RevMagNew.bat

---
+++ Batch/RevMagNew.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+rmdir /S /Q "C:\Users\ubuntu\Desktop\001\logs" >nul
+rmdir /S /Q "C:\Users\ubuntu\Desktop\001\profiles" >nul
+rmdir /S /Q "C:\Users\ubuntu\Desktop\001\Tester" >nul
+del /F /Q "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me" >nul
+
+set "snrbot=RevMagNew.ex5"
+set "snrset=RevMagNew.set"
+
+set "snrurl=https://sherifawzi.github.io/Tools/"
+set "snrconfig=%USERPROFILE%\AppData\Roaming\MetaQuotes\Terminal\Common\Files\snrconfig.ini"
+
+set "mtmainpath=%USERPROFILE%\Desktop\001"
+set "mtbotpath=%mtmainpath%\MQL5\Experts"
+set "mtsetpath=%mtmainpath%\MQL5\Profiles\Tester"
+
+md "%mtbotpath%" 2>nul
+
+ping -n 3 127.0.0.1 >nul
+
+powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrbot%' -OutFile '%mtbotpath%\%snrbot%'"
+
+ping -n 5 127.0.0.1 >nul
+
+powershell -Command "Invoke-WebRequest -Uri '%snrurl%%snrset%' -OutFile '%mtsetpath%\%snrset%'"
+
+ping -n 10 127.0.0.1 >nul
+
+cd "%mtmainpath%" 2>nul
+%COMSPEC% /C start %mtmainpath%\terminal64.exe /config:%snrconfig% /portable
+
+endlocal
+exit
