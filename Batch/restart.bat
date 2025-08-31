9f930bf96e0f72a66aa8d67c871a69bc55befcae
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2025-08-31 17:36:11 +0200

    Update restart.bat

--- Batch/restart.bat
+++ Batch/restart.bat
@@ -1,12 +1,13 @@
-        @echo off
-        :loop
-        if exist "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me" (
-            echo Restart signal file detected. Restarting the system...
-            ping -n 5 127.0.0.1 >nul
-            del "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me"
-            ping -n 15 127.0.0.1 >nul
-            shutdown /r /t 0
-            exit
-        )
-        timeout /t 180 /nobreak >nul
-        goto loop
+@echo off
+:loop
+if exist "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me" (
+    echo Restart signal file detected. Restarting the system...
+    timeout /t 5 /nobreak >nul
+    del "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me"
+    echo Waiting 30 seconds before restart...
+    timeout /t 30 /nobreak >nul
+    shutdown /r /t 0
+    exit
+)
+timeout /t 180 /nobreak >nul
+goto loop
