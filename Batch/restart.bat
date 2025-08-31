d81bc63075885e7d22525910f45d3586a8752787
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2025-08-31 17:40:23 +0200

    Update restart.bat

--- Batch/restart.bat
+++ Batch/restart.bat
@@ -1,9 +1,9 @@
 @echo off
 :loop
-if exist "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me" (
+if exist "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me" (
     echo Restart signal file detected. Restarting the system...
     timeout /t 5 /nobreak >nul
-    del "C:\Users\ubuntu\AppData\Roaming\MetaQuotes\Terminal\Common\Files\restart.me"
+    del "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me"
     echo Waiting 30 seconds before restart...
     timeout /t 30 /nobreak >nul
     shutdown /r /t 0
