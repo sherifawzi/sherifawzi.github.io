3bd0e5904baa2667193ef5280061ca6806da721d
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2025-09-15 13:39:14 +0200

    Update restart.bat

--- Batch/restart.bat
+++ Batch/restart.bat
@@ -1,13 +1,11 @@
 @echo off
-:loop
+
 if exist "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me" (
     echo Restart signal file detected. Restarting the system...
     timeout /t 5 /nobreak >nul
     del "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me"
-    echo Waiting 30 seconds before restart...
-    timeout /t 30 /nobreak >nul
-    shutdown /r /t 0
+    echo Waiting 20 seconds before restart...
+    timeout /t 20 /nobreak >nul
+    shutdown /r /t 20
     exit
 )
-timeout /t 180 /nobreak >nul
-goto loop
