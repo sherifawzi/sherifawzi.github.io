a6ad86835966208261de6417d354628c4ddd3174
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2026-01-19 16:05:38 +0100

    Update comments in snrtestsetup.sh for clarity

--- Batch/snrtestsetup.sh
+++ Batch/snrtestsetup.sh
@@ -8,35 +8,37 @@
    # https://api.telegram.org
    # http://3.66.106.21
 
-# 1. Install Desktop Environment (XFCE - lightweight and good for RDP)
+# Install Desktop Environment (XFCE - lightweight and good for RDP)
    sudo apt clean -y && sudo apt-get update && sudo apt-get upgrade -y
    sudo apt install -y xfce4 xfce4-goodies
 
-# 2. Install XRDP for Remote Desktop
+# Install XRDP for Remote Desktop
    sudo apt install -y xrdp
    sudo systemctl enable xrdp
    sudo systemctl start xrdp
 
-# 3. Configure XRDP to use XFCE
+# Configure XRDP to use XFCE
    echo xfce4-session > ~/.xsession
    sudo systemctl restart xrdp
 
-#4. Configure Firewall (if enabled)
+# Configure Firewall (if enabled)
    sudo ufw allow 3389/tcp
 
-# 5. Install Wine for MetaTrader 5
+# Install Wine for MetaTrader 5
    sudo dpkg --add-architecture i386
    sudo apt update
    sudo apt install -y wine64 wine32 winetricks
-   winetricks vcrun2015 corefonts
 
-# 5a. Set Wine to Windows 10 mode & Disable debug messages
+# Set Wine to Windows 10 mode & Disable debug messages
    export WINEPREFIX="$HOME/.wine"
    export WINEARCH=win64
    export WINEDEBUG=-all
+
+# Set Wine to Windows 10 mode & Disable debug messages
+   winetricks vcrun2015 corefonts
    winecfg
 
-# 6. Download and Install MetaTrader 5
+# Download and Install MetaTrader 5
    mkdir -p ~/mt5
    cd ~/mt5
    wget https://sherifawzi.github.io/Tools/mt5setup.exe
@@ -45,7 +47,7 @@
    wget https://sherifawzi.github.io/Tools/SNRC.set
    wget https://www.snrobotix.com/MT5/terminal64.exe
 
-# 7. Keep everything up to date before restart
+# Keep everything up to date before restart
    sudo apt clean -y && sudo apt-get update && sudo apt-get upgrade -y
 
 ###############################################################################
