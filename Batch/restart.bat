ee36c3eb77b023e67948248879bce9c1aaae78fb
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2026-01-20 20:29:54 +0100

    260120

--- Batch/restart.bat
+++ Batch/restart.bat
@@ -1,11 +1,10 @@
 @echo off
 
-if exist "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me" (
+if exist "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.txt" (
     echo Restart signal file detected. Restarting the system...
-    timeout /t 5 /nobreak >nul
-    del "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.me"
-    echo Waiting 20 seconds before restart...
-    timeout /t 20 /nobreak >nul
-    shutdown /r /t 20
+    timeout /t 10 /nobreak >nul
+    del "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.txt"
+    timeout /t 10 /nobreak >nul
+    shutdown /r /f /t 10
     exit
 )
