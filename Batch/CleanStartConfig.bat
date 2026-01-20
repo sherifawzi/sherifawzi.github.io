a394b4c4867b831d6b7fdd49aeb3d74019cbd4b2
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2026-01-20 23:13:47 +0100

    Add exit command to CleanStartConfig.bat

--- Batch/CleanStartConfig.bat
+++ Batch/CleanStartConfig.bat
@@ -3,3 +3,4 @@ rmdir /S /Q "profiles" >nul
 rmdir /S /Q "Tester" >nul
 set "snrconfig=%USERPROFILE%\AppData\Roaming\MetaQuotes\Terminal\Common\Files\configur.ini"
 %COMSPEC% /C start terminal64.exe /config:"%snrconfig%" /portable
+exit
