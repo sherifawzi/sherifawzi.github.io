4935686b84bf19c96d3bc6acb55114cb6fc85e7f
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2026-02-03 12:44:03 +0100

    Send Telegram notification on restart signal
    Added a Telegram notification before restarting the system.

--- Batch/restart.bat
+++ Batch/restart.bat
@@ -5,6 +5,8 @@ if exist "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.txt" (
     timeout /t 10 /nobreak >nul
     del "%APPDATA%\MetaQuotes\Terminal\Common\Files\restart.txt"
     timeout /t 10 /nobreak >nul
+    curl -s -X POST "https://api.telegram.org/bot8450507003:AAHhqJg_6x_ajStvx2_eoZRHnVIRpexzQc4/sendMessage" -d chat_id=-1003285305833 -d text="<b>WIN01 Server Restart</b>" -d parse_mode=HTML
     shutdown /r /f /t 10
     exit
 )
+
