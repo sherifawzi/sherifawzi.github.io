6f9bef1ed33371d43a25841a22ae18eb68dfcfc7
Sherifawzi <65125466+sherifawzi@users.noreply.github.com>
2026-02-05 15:20:31 +0100

    Delete Batch/CleanStartConfig.bat

--- Batch/CleanStartConfig.bat
+++
@@ -1,6 +0,0 @@
-rmdir /S /Q "logs" >nul
-rmdir /S /Q "profiles" >nul
-rmdir /S /Q "Tester" >nul
-set "snrconfig=%USERPROFILE%\AppData\Roaming\MetaQuotes\Terminal\Common\Files\configur.ini"
-%COMSPEC% /C start terminal64.exe /config:"%snrconfig%" /portable
-exit
